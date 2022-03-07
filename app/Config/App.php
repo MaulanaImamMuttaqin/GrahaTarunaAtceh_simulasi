52b7516cf9b1fdf4fe6a1954b1fcc8522132f436
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-03-07 12:29:56 +0700

    asdas

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,8 +23,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
-    // public $baseURL = 'http://GrahaTarunaAtceh.com';  //pake virtualhost
+    // public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://GrahaTarunaAtceh.com';  //pake virtualhost
     /**
      * --------------------------------------------------------------------------
      * Index File
