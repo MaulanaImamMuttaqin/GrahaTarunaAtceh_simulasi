1881d512201cc79b2636a3425ed26e62c4c5fe89
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-04-11 16:54:20 +0700

    update bug fixes

--- app/Controllers/OperatorApi.php
+++ app/Controllers/OperatorApi.php
@@ -650,9 +650,9 @@ class OperatorApi extends BaseController
                                 ->first();
         
         if($get_participant_test_result){
-            $get_participant_test_result["kecermatan"] = json_decode( $get_participant_test_result["kecermatan"], true);
-            $get_participant_test_result["kecerdasan"] = json_decode( $get_participant_test_result["kecerdasan"], true);
-            $get_participant_test_result["kepribadian"] = json_decode( $get_participant_test_result["kepribadian"], true);
+            $get_participant_test_result["kecermatan"] = isset($get_participant_test_result["kecermatan"]) ?  json_decode( $get_participant_test_result["kecermatan"] , true) : NULL;
+            $get_participant_test_result["kecerdasan"] = isset($get_participant_test_result["kecerdasan"]) ?  json_decode( $get_participant_test_result["kecerdasan"] , true) : NULL;
+            $get_participant_test_result["kepribadian"] = isset($get_participant_test_result["kepribadian"] )? json_decode(  $get_participant_test_result["kepribadian"], true) : NULL;
             return $this->respond(["data"=> $get_participant_test_result]);
         }else{
             return $this->fail(["error" => true], 400);
